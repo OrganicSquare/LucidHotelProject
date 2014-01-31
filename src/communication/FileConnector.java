9fcac4802c5a94f9a04c8d08f80525b03d754816
BenAllenUK <external@benallen.info>
2014-01-31 22:43:22 +0000

    Updated File Connector

--- src/communication/FileConnector.java
+++ src/communication/FileConnector.java
@@ -2,72 +2,72 @@ package communication;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectOutputStream;
 
 import communication.Util;
 public class FileConnector {
 	private static String ProgramFilesLocation = null; 
 	public static void main(String args[]){
 	
-	
+		loadGameFiles();
 	}
 	public static boolean loadGameFiles(){
+		boolean fileSuccess = false;
 		ProgramFilesLocation = documentLocation();
 		
-		boolean findLocation = createFolder("GameJJVassd");
+		boolean findLocation = createFolder("LucidHotel");
 		if(findLocation){
-			boolean findFile = createFile("userDetails.txt");
+			boolean findFile = createFile("LucidHotel" +File.separator +"userDetails.txt");
 			if(findFile){
-				
-				
+				String s = "hi";
+				Object[] fileData = new Object[]{s};
+				writeDataToFile("LucidHotel" +File.separator +"userDetails.txt",fileData);
+				fileSuccess = true;
 			}
 		}
 		
 		
-		return false;
+		return fileSuccess;
 	}
-	private static boolean writeDataToFile(String content, String fileName){
+	private static boolean writeDataToFile(String fileName, Object[] inputObject){
+		boolean fileSuccess = false;
+		FileOutputStream fileStream = null;
+		ObjectOutputStream outputStream = null;
+		try {
+			System.out.println("Writing data to " + ProgramFilesLocation + fileName);
+			fileStream = new FileOutputStream(ProgramFilesLocation+ fileName);
+			outputStream = new ObjectOutputStream(fileStream);
+			for(int i = 0; i< inputObject.length; i++){
+				outputStream.writeObject(inputObject[i]);
+			}
+			outputStream.close();
+			fileSuccess = true;
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter(fileName);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		BufferedWriter bw = new BufferedWriter(fw);
-		try {
-			bw.write(content);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			bw.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		
-		return false;
+		return fileSuccess;
 	}
 	private static boolean createFile(String fileName){
 		Boolean FileCreationSuccess = true;
 
-		File f = new File(ProgramFilesLocation + File.separator + fileName);
+		File f = new File(ProgramFilesLocation + fileName);
+		System.out.println("Created New file: " + ProgramFilesLocation + fileName);
 		if (!f.exists()) {
-			f.mkdirs(); 
+			
 			try {
 				f.createNewFile();
 			} catch (IOException e) {
 				Util.logC("Error Creating new file");
 				FileCreationSuccess = false;
 			}
-		} else {
-			FileCreationSuccess = false;
 		}
 		
 		return FileCreationSuccess;
@@ -76,7 +76,6 @@ public class FileConnector {
 	private static boolean createFolder(String folderName){
 		Boolean FileCreationSuccess = true;
 		File theDir = new File(ProgramFilesLocation + folderName);
-		ProgramFilesLocation += ProgramFilesLocation + File.separator;
 		  if (!theDir.exists()) {
 		    System.out.println("creating directory: " + ProgramFilesLocation + folderName);
 		    FileCreationSuccess = theDir.mkdir();  
